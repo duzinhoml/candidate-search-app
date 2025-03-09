95967fb919979307203cd32561b352cd2110f059
Mathew Lopez <mathewlopez10@gmail.com>
2025-03-08 21:19:57 -0500

    'process' solution and fix to vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,9 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   envDir: './environment',
   plugins: [react()],
+  server: {
+    port: process.env.PORT ? parseInt(process.env.PORT) : 3000,
+    host: '0.0.0.0',
+    allowedHosts: ["candidate-search-app-m557.onrender.com"]
+  },
 });
